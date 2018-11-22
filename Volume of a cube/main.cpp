8fa4c914f0a6ee615611a733be348e363e999a25
Shaquille Soekhlal <shaquille.soekhlal@student.uvs.edu>
2018-11-21 22:05:41 -0300

    added a space

--- Volume of a cube/main.cpp
+++ Volume of a cube/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]){
 	cout << "Input the length of the cube\n";
 	cin >> l;
 	v = l*l*l;
-	cout << "the volume of the cube is"<<v<<"\n";
+	cout << "the volume of the cube is "<<v<<"\n";
 	system("PAUSE");
 	return 0;
 }
