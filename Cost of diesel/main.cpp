a5776f123d34dd3050d42a84e441f89b43883d3a
Shaquille Soekhlal <shaquille.soekhlal@student.uvs.edu>
2018-11-26 22:05:21 -0300

    Fixes and cleanup

--- Cost of diesel/main.cpp
+++ Cost of diesel/main.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-	float eff, diesel, A, fuel, cost;
-	eff = 20.00;
-	diesel = 50.00;
-	A = 90.00;
+	float eff = 20.00, diesel = 50.00, A = 90, fuel, cost;
+	//eff = 20.00;
+	//diesel = 50.00;
+	//A = 90.00;
 	fuel = A / eff;
 	cost = diesel * fuel;
 	cout << "The trip would cost Rs "<<cost<<".\n";
--- Swap the variables/main.cpp
+++ Swap the variables/main.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-	string a, b, c;
+	int a, b, c;
 	cout << "Input 2 variables, a and b, seperated by a space.\n";
 	cin >> a >> b;
 	c = a;
 	a = b;
 	b = c;
 	cout << "The variables have been swapped\n";
-	cout << "a = "<<a<<"\n b = "<<b<<"\n";
+	cout << "a is now "<<a<<" and b is now "<<b<<"\n";
 	system("PAUSE");
 	return 0;
 }
